d322153d49af49a0d725f7ff0d2f5a75eb6d81b9
saravanan-zaigo <asanashik7@gmail.com>
2021-06-11 17:29:39 +0530

    react-query used for fetching data and data displayed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Main from './components/currency_converter/Main';
+import UsingQuery from './components/react_query_practice/UsingQuery';
 
 function App() {
   return (
     <div>
-      <Main />
+      {/* <Main /> */}
+      <UsingQuery />
     </div>
   );
 }
